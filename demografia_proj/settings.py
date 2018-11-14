4ae6ceee418086d82d593d0a26eea400ed880aba
Ch6pe <jgonzalezo7@uniminuto.edu.co>
2018-11-14 18:47:17 -0500

    V2

--- demografia_proj/settings.py
+++ demografia_proj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webLogin'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,22 @@ WSGI_APPLICATION = 'demografia_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DBDemografia',
+        'USER' : 'userDemografia',
+        'PASSWORD' : 'demografia123',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+        'CHARSET' : 'UTF-8'
     }
 }
 
